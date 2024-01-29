d8aea6b30131557e9b9a484910b4fc6c260b9d97
Bouba10 <72755006+Bouba10@users.noreply.github.com>
2024-01-30 00:24:46 +0100

    commit final

--- MapKit_App/LocationDetailsView.swift
+++ MapKit_App/LocationDetailsView.swift
@@ -6,13 +6,101 @@
 //
 
 import SwiftUI
+import MapKit
 
 struct LocationDetailsView: View {
+    @Binding  var  mapSelection : MKMapItem?
+    @Binding var show : Bool
+    @Binding var getDirections : Bool
+    
+    @State private var  lookAroundScene : MKLookAroundScene?
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            HStack {
+                VStack(alignment: .leading){
+                    Text(mapSelection?.placemark.name ?? "")
+                        .font(.title2)
+                        .fontWeight(.semibold)
+                    
+                    Text(mapSelection?.placemark.title ?? "")
+                        .font(.footnote)
+                        .foregroundStyle(.gray)
+                        .lineLimit(2)
+                        .padding(.trailing)
+                    
+                }
+                Spacer()
+                
+                Button{
+                    show.toggle()
+                    mapSelection = nil
+                }label: {
+                    Image(systemName: "xmark.circle.fill")
+                        .resizable()
+                        .frame(width: 24, height: 24)
+                        .foregroundStyle(.gray, Color(.systemGray6))
+                }
+                
+            }
+            
+            if let scene = lookAroundScene {
+                LookAroundPreview(initialScene: scene)
+                    .frame(height: 200)
+                    .clipShape(RoundedRectangle(cornerRadius: 10))
+                    .padding()
+            }else{
+                ContentUnavailableView("No preview availabe", systemImage: "eye.slash" )
+            }
+            
+            HStack(spacing: 24){
+                Button{
+                    if let mapSelection {
+                        mapSelection.openInMaps()
+                    }
+                }label: {
+                    Text("open in Maps")
+                        .font(.headline)
+                        .foregroundStyle(.white)
+                        .frame(width: 170, height: 48)
+                        .background(.green)
+                        .clipShape(RoundedRectangle(cornerRadius: 12))
+                }
+                
+                Button{
+                   getDirections = true
+                    show = false
+                }label: {
+                    Text("Get Directions")
+                        .font(.headline)
+                        .foregroundStyle(.white)
+                        .frame(width: 170, height: 48)
+                        .background(.blue)
+                        .clipShape(RoundedRectangle(cornerRadius: 12))
+                }
+            }
+        }
+        .onAppear{
+            fetchLookAroundPreview()
+        }
+        .onChange(of: mapSelection) {
+            fetchLookAroundPreview()
+        }
+        .padding()
+    }
+}
+
+extension LocationDetailsView {
+    func fetchLookAroundPreview(){
+        if let mapSelection {
+            lookAroundScene = nil
+            Task{
+                let request = MKLookAroundSceneRequest(mapItem: mapSelection)
+                lookAroundScene = try? await  request.scene
+            }
+        }
     }
 }
 
 #Preview {
-    LocationDetailsView()
+    LocationDetailsView(mapSelection: .constant(nil), show: .constant(false), getDirections: .constant(false))
 }
