10262f5612149a909f7f4ec8ee8fbe66193bee23
Bouba10 <72755006+Bouba10@users.noreply.github.com>
2024-01-30 00:24:17 +0100

    final commmit v1.o

---
+++ MapKit_App/LocationDetailsView.swift
@@ -0,0 +1,18 @@
+//
+//  LocationDetailsView.swift
+//  MapKit_App
+//
+//  Created by Boubacar sidiki barry on 29.01.24.
+//
+
+import SwiftUI
+
+struct LocationDetailsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LocationDetailsView()
+}
