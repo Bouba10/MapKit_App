e150dd22072bfc0857a81e97c24b77b118f31bc7
Bouba10 <72755006+Bouba10@users.noreply.github.com>
2024-01-29 15:20:41 +0100

    Initial Commit

---
+++ MapKit_App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MapKit_App
+//
+//  Created by Boubacar sidiki barry on 29.01.24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MapKit_App/MapKit_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapKit_AppApp.swift
+//  MapKit_App
+//
+//  Created by Boubacar sidiki barry on 29.01.24.
+//
+
+import SwiftUI
+
+@main
+struct MapKit_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
